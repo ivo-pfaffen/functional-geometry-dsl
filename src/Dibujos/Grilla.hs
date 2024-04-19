8f0702bc4e8a6edfb70ad67a9c52701297dae908
augustolallana <lallanaaugusto@gmail.com>
2024-04-18 21:23:57 -0300

    Agregar estructura inicial de grilla.
    Co-authored-by: Ivo Pfaffen ivopfaffen@gmail.com

--- src/Dibujos/Grilla.hs
+++ src/Dibujos/Grilla.hs
@@ -1,8 +1,11 @@
 module Dibujos.Grilla (
-    grilla
+    grilla,
+    grillaConf
 ) where
 
-import Dibujo (Dibujo, juntar, apilar)
+import Dibujo (Dibujo, figura, juntar, apilar)
+import Graphics.Gloss ( Picture, text, color, black, red )
+import FloatingPic (Output, Conf(..))
 
 row :: [Dibujo a] -> Dibujo a
 row [] = error "row: no puede ser vacío"
@@ -16,3 +19,37 @@ column (d:ds) = apilar (fromIntegral $ length ds) 1 d (column ds)
 
 grilla :: [[Dibujo a]] -> Dibujo a
 grilla = column . map row
+
+data Color = Negro | Rojo
+    deriving(Show, Eq)
+
+data BasicaSinColor = Parentesis | Numero | Coma
+    deriving(Show, Eq)
+
+type Basica = (BasicaSinColor, Color)
+
+colorear :: Color -> Picture -> Picture
+colorear Negro = color black
+colorear Rojo = color red
+
+interpBasicaSinColor :: Output BasicaSinColor
+interpBasicaSinColor Parentesis x y w = text "("
+
+interpBas :: Output Basica
+interpBas (b, c) x y w = colorear c $ interpBasicaSinColor b x y w
+
+parentesisNegro :: BasicaSinColor -> Dibujo Basica
+parentesisNegro b = figura (b, Negro)
+
+generarGrilla :: Dibujo Basica
+generarGrilla = grilla [
+    [parentesisNegro Parentesis, parentesisNegro Parentesis],
+    [parentesisNegro Parentesis, parentesisNegro Parentesis]
+    ]
+
+grillaConf :: Conf
+grillaConf = Conf {
+    name = "Grilla"
+    , pic = generarGrilla
+    , bas = interpBas
+}
--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 -- import Dibujos.Ejemplo (ejemploConf)
 import Dibujos.Feo (feoConf)
+import Dibujos.Grilla (grillaConf)
 -- import Dibujos.Cuadrados(cuadConf)
 import FloatingPic (Conf (..))
 import Interp (initial)
@@ -14,7 +15,7 @@ import InterpSVG (ConfSVG, initialSVG', simpleSVG)
 -- Lista de configuraciones de los dibujos
 configs :: [Conf]
 -- configs = [ejemploConf, feoConf,cuadConf 3]
-configs = [feoConf]
+configs = [feoConf, grillaConf]
 
 configsH :: [ConfH]
 configsH = map (\(Conf n p _) -> simpleHaha n p) configs
